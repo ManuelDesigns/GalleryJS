43c548e0f08cb37de0893f6dfae813c487b6b1c2
Unknown <mpaniagua7@mail.csuchico.edu>
2017-04-27 11:57:23 -0700

    slight update

--- gallery.js
+++ gallery.js
@@ -1,6 +1,12 @@
+var chico = ["Madison Bear","Burgers and Brew","In & Out","Bidwell Park","WREC"]
 
+var current
 
 function shuffleGallery()
 {
+
+var random = Math.floor(Math.random() * chico.length);
+console.log(random);
+
 
 }
